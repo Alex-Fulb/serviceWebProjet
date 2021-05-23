2199c6bc5eeb0ad91894bc1410ce91a9f46ada47
FulbertAlex <alexfulb94@hotmail.fr>
2021-05-23 19:03:53 +0200

    v.2
    creation fictive d'un bot sur la page d'accueil ( aucun boutton fonctionnel, aucune insertion dans une bdd avec mongoDB)

--- chatBotProjet/backend/routes/webSiteRoutes.js
+++ chatBotProjet/backend/routes/webSiteRoutes.js
@@ -1,31 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-
 // --------- GET -----------
 
 // Home page
 router.get('/', function(req, res) {
-  if (req.cookies['dataUser']=== undefined) res.redirect('/') 
-  else res.render('../../views/pages/index.ejs', {name: req.cookies['dataUser']});
+  if (req.cookies['dataUser'] === undefined)
+    res.redirect('/')
+    else res.render(
+        '../../views/pages/index.ejs', {name: req.cookies['dataUser']
+      });
 });
 
 // Page About
 router.get('/about', function(req, res) {
   // if (req.cookies['dataUser']=== undefined) res.redirect('/')
-  // else 
+  // else
   res.render('../../views/pages/about.ejs');
 });
 
 // Deconnexion
 router.get('/disconnect', function(req, res) {
-  if (req.cookies['dataUser']=== undefined) res.redirect('/')
-  else {
-    res.clearCookie('dataUser')
+  if (req.cookies['dataUser'] === undefined)
     res.redirect('/')
-  }
+    else {
+      res.clearCookie('dataUser')
+      res.redirect('/')
+    }
 });
 
+// ===========================================================================================
+
+
+
+// ------- POST --------
+
+// Page About
+router.post('/createBot', function(req, res) {
+  console.log('ah j\'ai reçu', req.body.nom)
+  // res.cookie('', newUser.name)
+  //res.render('../../views/pages/index.ejs', {mot: req.cookies['dataUser'], bot :"ok"});
+  res.send(req.body.nom);
+});
 
 
 
--- chatBotProjet/backend/server.js
+++ chatBotProjet/backend/server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(cookieParser())
+app.set('view engine', 'ejs');
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(
