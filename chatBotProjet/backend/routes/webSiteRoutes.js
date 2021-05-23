d41ab520b483431377c05f3c2ee3d7957c94215d
FulbertAlex <alexfulb94@hotmail.fr>
2021-05-23 10:52:07 +0200

    v.1
    Modification de la page d'accueil pour pouvoir travailler sur les bots

--- chatBotProjet/backend/routes/webSiteRoutes.js
+++ chatBotProjet/backend/routes/webSiteRoutes.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+
+// --------- GET -----------
+
 // Home page
 router.get('/', function(req, res) {
-
   if (req.cookies['dataUser']=== undefined) res.redirect('/') 
   else res.render('../../views/pages/index.ejs', {name: req.cookies['dataUser']});
-  
 });
 
 // Page About
 router.get('/about', function(req, res) {
-  if (req.cookies['dataUser']=== undefined) res.redirect('/')
-  else res.render('../../views/pages/about.ejs');
+  // if (req.cookies['dataUser']=== undefined) res.redirect('/')
+  // else 
+  res.render('../../views/pages/about.ejs');
 });
+
+// Deconnexion
+router.get('/disconnect', function(req, res) {
+  if (req.cookies['dataUser']=== undefined) res.redirect('/')
+  else {
+    res.clearCookie('dataUser')
+    res.redirect('/')
+  }
+});
+
 
 
 
